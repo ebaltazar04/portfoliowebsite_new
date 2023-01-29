489929972d92b1786456a96afaa76849c3c88d80
Baltazar <ebaltazar1145@gmail.com>
2023-01-29 14:26:33 -0800

    add JS code to toggle image in about me sction and styled contact page

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-// let skillSets = document.getElementById('skill');
-// let popOne = skillSets.getElementsByTagName('div')[0];
-// let imageHTML = popOne.getElementsByTagName('img')[0];
 
-
-// function skillOne() {
-//     popOne.style.height = '13rem';
-//     popOne.style.width = '13rem';
-//     imageHTML.style.height = '5rem';
-//     imageHTML.style.width = '50%';
-// }
-
-// function removePopOne() {
-//     popOne.style.height = '11rem';
-//     popOne.style.width = '9rem';
-//     imageHTML.style.height = '4rem';
-//     imageHTML.style.width = '50%';
-// }
-
-// popOne.addEventListener('mouseover', skillOne);
-// popOne.addEventListener('mouseleave', removePopOne);
-
-// console.log(skillSets);
-// console.log(pop);
-// console.log(popOne);
+function changeImage() {
+    let displayImage = document.getElementById('image1')
+    if(displayImage.src.match('images/desktop.jpg')){
+        displayImage.src = 'images/Petunia.jpg'
+    } else {
+        displayImage.src = 'images/desktop.jpg'
+    }
+}
 
